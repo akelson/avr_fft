3a1403a1f409718f7cd81330599679a8adf3f717
Adam Kelson <adam@adamkelson.com>
2019-02-27 22:39:26 -0700

    Formatting cleanup.

--- FixedPoint.hpp
+++ FixedPoint.hpp
@@ -12,12 +12,12 @@ struct FixedPoint
   }
 
   template<typename T1, int F1>
-    constexpr FixedPoint(const FixedPoint<T1, F1>& other) :
-      value_(F1 > F ? (other.value_ >> (F1 - F)) :
-          (other.value_ << (F - F1)))
-      {
-        // Empty
-      }
+  constexpr FixedPoint(const FixedPoint<T1, F1>& other) :
+    value_(F1 > F ? (other.value_ >> (F1 - F)) :
+                    (other.value_ << (F - F1)))
+  {
+    // Empty
+  }
 
   constexpr FixedPoint(const double value) :
     value_(static_cast<T>(value * (1<<F)))
@@ -51,18 +51,18 @@ struct result_types<int16_t, F1, F2>
   using div = void;
 };
 
-  template<typename T, int F>
+template<typename T, int F>
 FixedPoint<T,F> operator+ (
-    const FixedPoint<T,F>& a, const FixedPoint<T,F>& b)
+  const FixedPoint<T,F>& a, const FixedPoint<T,F>& b)
 {
   FixedPoint<T,F> ret;
   ret.value_ = a.value_ + b.value_;
   return ret;
 }
 
-  template<typename T, int F>
+template<typename T, int F>
 FixedPoint<T,F> operator- (
-    const FixedPoint<T,F>& a, const FixedPoint<T,F>& b)
+  const FixedPoint<T,F>& a, const FixedPoint<T,F>& b)
 {
   FixedPoint<T,F> ret;
   ret.value_ = a.value_ - b.value_;
@@ -70,7 +70,7 @@ FixedPoint<T,F> operator- (
 }
 
 // Trailing return type! Heck yeah!
-  template<typename T, int F1, int F2>
+template<typename T, int F1, int F2>
 auto operator*( const FixedPoint<T,F1>& a, const FixedPoint<T,F2>& b)
   -> typename result_types<T,F1,F2>::mul 
 {
@@ -79,9 +79,9 @@ auto operator*( const FixedPoint<T,F1>& a, const FixedPoint<T,F2>& b)
   return ret;
 }
 
-  template<typename T, int F>
+template<typename T, int F>
 FixedPoint<T,F> operator/ (
-    const FixedPoint<T,F>& a, const FixedPoint<T,F>& b)
+  const FixedPoint<T,F>& a, const FixedPoint<T,F>& b)
 {
   FixedPoint<T,F> ret;
   ret.value_ = (a.value_ / b.value_) << F;
@@ -89,7 +89,7 @@ FixedPoint<T,F> operator/ (
 }
 
 #ifndef AVR
-  template<typename T, int F>
+template<typename T, int F>
 std::ostream& operator<< (std::ostream& os, const FixedPoint<T,F>& obj)
 {
   os << obj.toDouble();
